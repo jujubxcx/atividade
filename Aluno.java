0f00b86a93d08913516833cff56716a3e2112d4a
Juliana M. Cruz <148486432+jujubxcx@users.noreply.github.com>
2025-09-22 10:19:29 -0300

    Add files via upload

--- Aluno.java
+++ Aluno.java
@@ -1,16 +1,19 @@
 public class Aluno {
+    // Atributos
     String nome;
     int matricula;
     double notaAv1;
     double notaAv2;
-  
+
+    // Construtor
     public Aluno(String nome, int matricula, double notaAv1, double notaAv2) {
         this.nome = nome;
         this.matricula = matricula;
         this.notaAv1 = notaAv1;
         this.notaAv2 = notaAv2;
     }
-  
+
+    // Métodos Getters e Setters
     public String getNome() {
         return nome;
     }
@@ -35,6 +38,7 @@ public class Aluno {
         this.notaAv2 = notaAv2;
     }
 
+    // Mostrar os dados do aluno
     public void mostrarDados() {
         System.out.println("Nome: " + nome);
         System.out.println("Matrícula: " + matricula);
@@ -44,10 +48,12 @@ public class Aluno {
         System.out.println("----------------------");
     }
 
+    // Calcular a média
     public double calcularMedia() {
         return (notaAv1 + notaAv2) / 2;
     }
 
+    // Verificar aprovação (Regra: média >= 7 está aprovado, senão reprovado)
     public void verificarAprovacao() {
         double media = calcularMedia();
         if (media >= 7) {
